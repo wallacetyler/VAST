9f0479f7ec7d7f2d75f930f1e727a587689fc83a
Tyler Wallace <tyler@ucf.edu>
2019-11-01 17:01:06 -0400

    Abstracted out some functionality of the Services

--- vast_oop.py
+++ vast_oop.py
@@ -13,8 +13,7 @@ course = canvas.get_course(course_id)
 
 
 class ResourceProvider:
-    def __init__(self, get_function, enabled = True):
-        self.get_function = get_function
+    def __init__(self, enabled = True):
         self.enabled = enabled
 
     def fetch(self):
@@ -25,19 +24,30 @@ class ResourceProvider:
         return resources
 
     def get_resources(self):
-        raise NotImplementedError
+        if not self.function:
+            raise Exception('Service has no function.')
+
+        items = self.function()
+        content = []
+        for item in items:
+            content.append((
+                getattr(item, self.field),
+                item.html_url
+            ))
+
+        return content
 
 
 class SyllabusService(ResourceProvider):
     def get_resources(self):
-        syllabus = self.get_function(course_id, include='syllabus_body')
+        syllabus = canvas.get_course(course_id, include='syllabus_body')
         url = '{}/courses/{}/assignments/syllabus'.format(api_url, course_id)
         return (syllabus.syllabus_body, url)
 
 
 class AnnouncementService(ResourceProvider):
     def get_resources(self):
-        announcements = self.get_function(only_announcements=True)
+        announcements = course.get_discussion_topics(only_announcements=True)
         content = []
         for announcement in announcements:
             content.append((announcement.message, announcement.html_url))
@@ -46,7 +56,7 @@ class AnnouncementService(ResourceProvider):
 
 class ModuleService(ResourceProvider):
     def get_resources(self):
-        modules = self.get_function()
+        modules = course.get_modules()
         content = []
         for module in modules:
             for item in module.get_module_items(include='content_details'):
@@ -56,26 +66,18 @@ class ModuleService(ResourceProvider):
 
 
 class AssignmentService(ResourceProvider):
-    def get_resources(self):
-        assignments = self.get_function()
-        content = []
-        for assignment in assignments:
-            content.append((assignment.description, assignment.html_url))
-        return content
+    function = course.get_assignments
+    field = "description"
 
 
 class DiscussionService(ResourceProvider):
-    def get_resources(self):
-        discussions = self.get_function()
-        content = []
-        for discussion in discussions:
-            content.append((discussion.message, discussion.html_url))
-        return content
-
+    function = course.get_discussion_topics
+    field = "message"
+    
 
 class PageService(ResourceProvider):
     def get_resources(self):
-        pages = self.get_function()
+        pages = course.get_pages()
         content = []
         for page in pages:
             content.append((course.get_page(page.url), page.html_url))
@@ -83,15 +85,15 @@ class PageService(ResourceProvider):
 
 
 RESOURCES = [
-    SyllabusService(get_function = canvas.get_course, enabled = True),
-    AnnouncementService(get_function = course.get_discussion_topics, enabled = True),
-    ModuleService(get_function = course.get_modules, enabled = True),
-    AssignmentService(get_function = course.get_assignments, enabled = True),
-    DiscussionService(get_function = course.get_discussion_topics, enabled = True),
-    PageService(get_function = course.get_pages, enabled = True)
+    SyllabusService(enabled = True),
+    AnnouncementService(enabled = False),
+    ModuleService(enabled = False),
+    AssignmentService(enabled = False),
+    DiscussionService(enabled = False),
+    PageService(enabled = False)
 ]
 
 for resource in RESOURCES:
     content = resource.fetch()
-    media, success = resource.parse()
+    # media, success = resource.parse()
     import pdb; pdb.set_trace()
